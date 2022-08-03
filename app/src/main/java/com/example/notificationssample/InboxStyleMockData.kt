7fb280925ada544bd7cc5604a7bf64c6beac433c
Eduardo Vieira <edu_vieira@hotmail.com>
2022-08-03 13:53:25 -0300

    Simple notification setup

---
+++ app/src/main/java/com/example/notificationssample/InboxStyleMockData.kt
@@ -0,0 +1,52 @@
+package com.example.notificationssample
+
+import android.app.NotificationManager
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+
+object InboxStyleMockData {
+    const val mContentTitle = "5 new emails"
+    const val mContentText = "from Janet, Jon, Tony +2 more"
+    const val mNumberOfNewEmails = 5
+    const val mPriority = NotificationCompat.PRIORITY_DEFAULT
+
+    const val mBigContentTitle = "5 new emails from Jane, Jay, Alex +2"
+    const val mSummaryText = "New emails"
+
+    fun mIndividualEmailSummary(): ArrayList<String> {
+        val list = ArrayList<String>()
+
+        list.add("Janet Farber - B-day Party is here...")
+        list.add("Jon Walker - There's a pelican on the server!")
+        list.add("Tony Chang - Check this on  out...")
+        list.add("Amanda Mendez - Check in email?")
+        list.add("Johnathan Cooper - Dinner later....")
+
+        return list
+    }
+
+    fun mParticipants(): ArrayList<String> {
+        // If the phone is in "Do not disturb mode, the user will still be notified if
+        // the user(s) is starred as a favorite.
+        val list = ArrayList<String>()
+
+        list.add("Janet Farber")
+        list.add("Jon Walker")
+        list.add("Tony Chang")
+        list.add("JAmanda Mendez")
+        list.add("Johnathan Cooper")
+
+        return list
+    }
+
+    const val mChannelId = "channel_email_1"
+    const val mChannelName = "Sample Email"
+
+    // The user-visible description of the channel.
+    const val mChannelDescription = "Sample Email Notifications"
+    @RequiresApi(Build.VERSION_CODES.N)
+    const val mChannelImportance = NotificationManager.IMPORTANCE_DEFAULT
+    const val mChannelEnableVibrate = true
+    const val mChannelLockscreenVisibility = NotificationCompat.VISIBILITY_PRIVATE
+}
--- app/src/main/java/com/example/notificationssample/MainActivity.kt
+++ app/src/main/java/com/example/notificationssample/MainActivity.kt
@@ -1,11 +1,94 @@
 package com.example.notificationssample
 
+import android.annotation.SuppressLint
+import android.app.Notification
+import android.app.PendingIntent
+import android.content.Intent
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private val NOTIFICATION_ID = 888
+    private lateinit var mNotificationManagerCompat: NotificationManagerCompat
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mNotificationManagerCompat = NotificationManagerCompat.from(this@MainActivity)
+
+        val btnInboxStyleNotification = findViewById<Button>(R.id.btn_inbox_style)
+        btnInboxStyleNotification.setOnClickListener(this@MainActivity)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btn_inbox_style -> {
+                generateInboxStyleNotification()
+                return
+            }
+        }
+    }
+
+    @Suppress("DEPRECATION")
+    @SuppressLint("UnspecifiedImmutableFlag")
+    private fun generateInboxStyleNotification() {
+        val notificationChannelId: String =
+            NotificationUtil().createInboxStyleNotificationChannel(this)
+
+        val inboxStyle =
+            NotificationCompat
+                .InboxStyle()
+                .setBigContentTitle(InboxStyleMockData.mBigContentTitle)
+                .setSummaryText(InboxStyleMockData.mSummaryText)
+
+        for (summary in InboxStyleMockData.mIndividualEmailSummary()) {
+            inboxStyle.addLine(summary)
+        }
+
+        val mainIntent = Intent(this, MainActivity::class.java)
+
+        val mainPendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            mainIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val notificationCompatBuilder = NotificationCompat.Builder(
+            applicationContext,
+            notificationChannelId
+        )
+
+        notificationCompatBuilder
+            .setStyle(inboxStyle)
+            .setContentTitle(InboxStyleMockData.mContentTitle)
+            .setContentText(InboxStyleMockData.mContentText)
+            .setSmallIcon(R.drawable.ic_email)
+            .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_person))
+            .setContentIntent(mainPendingIntent)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+            .setColor(
+                ContextCompat.getColor(
+                    applicationContext,
+                    R.color.primary_color_dark
+                )
+            )
+            .setSubText(InboxStyleMockData.mNumberOfNewEmails.toString())
+            .setCategory(Notification.CATEGORY_EMAIL).priority = InboxStyleMockData.mPriority
+
+        for (name in InboxStyleMockData.mParticipants()) {
+            notificationCompatBuilder.addPerson(name)
+        }
+
+        val notification = notificationCompatBuilder.build()
+
+        mNotificationManagerCompat.notify(NOTIFICATION_ID, notification)
     }
 }
